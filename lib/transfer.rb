07d27e8ed5d8a4ff90ea1ea6979ec938eed11a87
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 18:59:35 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,6 +14,14 @@ class Transfer
   end
 
   def execute_transaction
+    if valid? && @sender.balance >= @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
   end
 
   def reverse_transfer
