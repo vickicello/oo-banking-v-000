e1d6181706a350b3e05b8c3022aec6ff205f4fe0
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 19:00:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,11 @@ class Transfer
   end
 
   def reverse_transfer
+    if @status == "complete"
+       @receiver.balance -= @amount
+       @sender.balance += @amount
+       @status = "reversed"
+     end
   end
 
 
