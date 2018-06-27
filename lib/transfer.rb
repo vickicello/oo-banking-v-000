9966c506bb3e60a360e065de23d497aa57b911eb
Vicki Hamm <vicki.cello@gmail.com>
2018-06-27 18:56:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,17 +2,15 @@ class Transfer
 
   attr_accessor :sender, :receiver, :status, :amount
 
-  @@all = []
-
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
-    @amount = 50
-    @@all << self
+    @amount = amount
   end
 
   def valid?
+    
   end
 
   def execute_transaction
